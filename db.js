fd68fbb5900b545d35b9b7f28f153291dbef6db0
Aggie Skorska <57585879+agiskorska@users.noreply.github.com>
2022-07-22 15:20:29 +0100

    Create db.js
    Add a simple DB stracture.

---
+++ db.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+  id: 0,
+  post: '',
+  reaction: ['heart', 'like', 'dislike'],
+  comments: [
+    {content: 'I don\'t like this', isGiphy: false}, // <p> I don't like this </p>
+    {content: 'You know nothing, I love it', isGiphy: false },
+    {content: 'http://nbc.com/zoeys-extraordinary-playlist', isGiphy: true} // <img src="http://nbc.com/zoeys-extraordinary-playlist">
+  ]
+  },
+    {
+  id: 1,
+  post: '',
+  reaction: ['heart', 'like', 'dislike'],
+  comments: [
+    {content: 'I don\'t like this', isGiphy: false}, // <p> I don't like this </p>
+    {content: 'You know nothing, I love it', isGiphy: false },
+    {content: 'http://nbc.com/zoeys-extraordinary-playlist', isGiphy: true} // <img src="http://nbc.com/zoeys-extraordinary-playlist">
+  ]
+  },
+  
+ ]
+  
+  
+  
