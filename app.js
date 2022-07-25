1d212a412ece139681cb8ac0e3b8549caa9f5276
madCakes <jason.pbrooks@hotmail.com>
2022-07-25 11:19:18 +0100

    add get post by id

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const cors = require("cors");
 
 const data = require("./db");
 
+// Home route
 app.get("/", (req, res) => res.send("Welcome to the Unfiltered API!"));
+
+// Post route
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  const post = data.filter((post) => post["id"] == id);
+
+  res.json({
+    post,
+  });
+});
+
+// Comment route
+app.get("/posts", (req, res) => res.send(data));
 
 module.exports = app;
